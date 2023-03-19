215127f0875b594765611f377edb2888f90b9e81
israehamoumi <israe.hamoumi@gmail.com>
2023-03-19 15:16:52 -0700

    israe

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,8 @@ int main(void)
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-if (ch != 'e' && ch != 'q')	
+if (ch != 'e' && ch != 'q')
 putchar(ch);
 }
-for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);
 return (0);
 }
