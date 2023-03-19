d76de013db45d1734720b6d3e5214625eaa53501
israehamoumi <israe.hamoumi@gmail.com>
2023-03-19 16:23:24 -0700

    israe

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,18 @@
 int main(void)
 {
 int digit1, digit2;
+int is_last_combination;
 for (digit1 = 0; digit1 < 9; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar(digit1 + '0');
 putchar(digit2 + '0');
-if (digit1 != 8 || digit2 != 9)
+is_last_combination = (digit1 == 8 && digit2 == 9);
+if (!is_last_combination)
+{
 putchar(',');
-putchar(' ');
+}
 }
 }
 putchar('\n');
