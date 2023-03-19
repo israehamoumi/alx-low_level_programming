aa6d0b1c082e0659a757736ea304d2615d580f5f
israehamoumi <israe.hamoumi@gmail.com>
2023-03-19 14:43:37 -0700

    israe

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,12 @@ int main(void)
 char letter = 'a';
 while (letter <= 'z')
 {
-if (letter != 'q' || letter != 'e')
+if (letter != 'q' && letter != 'e')
 {
 putchar(letter);
+}
 letter++;
-}}
+}
 putchar('\n');
 return (0);
 }
