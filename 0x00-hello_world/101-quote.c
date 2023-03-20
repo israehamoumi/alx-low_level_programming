35b162c94245551835119a03a8c5a168ec1a64e7
israehamoumi <israe.hamoumi@gmail.com>
2023-03-20 06:09:24 -0700

    israe

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return 1;
+return (1);
 }
