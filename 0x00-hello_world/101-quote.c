88167011be3242cb0538d2181bb90a99ae74c1d3
israehamoumi <israe.hamoumi@gmail.com>
2023-03-20 06:03:03 -0700

    israe

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include <unistd.h>
+#include <string.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always (1) (Error)
  */
 int main(void)
 {
-char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, msg, 59);
-return 1;
+char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = 0;
+while (quote[len])
+len++;
+write(2, quote, len);
+return (1);
 }
