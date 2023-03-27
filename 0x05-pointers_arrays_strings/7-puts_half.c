7360b16208de6de7b693e703edf596d2e9d66ef7
israehamoumi <israe.hamoumi@gmail.com>
2023-03-27 15:37:30 -0700

    isrea

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,32 +1,18 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 /**
- * puts_half - prints the second half of a string, followed by a new line
- *
- * @str: the string to print
+ * puts_half - prints the second half of a string
+ * @str: input string
  */
-void puts_half(char *str);
-int main(void)
-{
-char *str = "Lorem ipsum";
-puts_half(str);
-return 0;
-}
 void puts_half(char *str)
 {
 int len = strlen(str);
 int i, start;
 if (len % 2 == 0)
-{
 start = len / 2;
-}
 else
-{
 start = (len - 1) / 2;
-}
 for (i = start; i < len; i++)
-{
 putchar(str[i]);
-}
 putchar('\n');
 }
