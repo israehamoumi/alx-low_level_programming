f1b55ccc481cf281e60ff4f158d880cebd31f89d
israehamoumi <israe.hamoumi@gmail.com>
2023-03-27 11:07:03 -0700

    israe

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 /**
-*print_array - Prints an array of integers
-*
-*@a: Pointer to the array of integers
-*@n: Number of elements in the array
-*
-*Description: This function prints the elements of an integer array.
-*Each element is separated by a comma, followed by a space.
-*
-*Return: Nothing
-*/
+ * reset_to_98 - Updates the value a pointer points to to 98.
+ *
+ * @n: A pointer to an int.
+ *
+ * Return: Nothing.
+ */
 void reset_to_98(int *n)
 {
 *n = 98;
