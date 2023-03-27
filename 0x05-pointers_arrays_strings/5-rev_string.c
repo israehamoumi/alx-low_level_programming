e75da15c1ff6ba5145f8a9220747e8cd21fbdb5b
israehamoumi <israe.hamoumi@gmail.com>
2023-03-27 12:33:23 -0700

    israe

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,8 @@ void rev_string(char *s)
 int len = strlen(s);
 int i;
 char temp;
-for (i = 0; i < len / 2; i++) {
+for (i = 0; i < len / 2; i++)
+{
 temp = s[i];
 s[i] = s[len - i - 1];
 s[len - i - 1] = temp;
