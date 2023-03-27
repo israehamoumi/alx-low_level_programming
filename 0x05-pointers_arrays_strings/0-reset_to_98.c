153468436c61950fc428e3b0e202f36a56c3f998
israehamoumi <israe.hamoumi@gmail.com>
2023-03-27 11:01:39 -0700

    israe

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_array - Prints an array of integers
+ *
+ * @a: Pointer to the array of integers
+ * @n: Number of elements in the array
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
