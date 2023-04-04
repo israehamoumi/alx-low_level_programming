22966693a58cae7167c0432027137a028d5f5701
israehamoumi <israe.hamoumi@gmail.com>
2023-04-04 06:11:30 -0700

    israe

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 /**
- * set_string - sets the value of a pointer to a char.
- * @s: double pointer to the string to be set
- * @to: pointer to the string to be copied
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer to string to set
+ * @to: pointer to string to copy
  *
  * Return: void
  */
 void set_string(char **s, char *to)
 {
 *s = to;
+}
+
+/**
+ * main - tests the set_string function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char *s0 = "Bob Dylan";
+char *s1 = "Robert Allen";
+printf("%s, %s\n", s0, s1);
+set_string(&s1, s0);
+printf("%s, %s\n", s0, s1);
+return (0);
 }
