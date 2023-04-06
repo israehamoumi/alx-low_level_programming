61c5281a4573c5198aee2a8672f246faeb5bf3a4
israehamoumi <israe.hamoumi@gmail.com>
2023-04-06 14:57:05 -0700

    israe

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,35 @@
-#include <stdio.h>
+/**
+ * _strlen - calculates the length of a string
+ * @s: the string to calculate the length of
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (_strlen(s + 1) + 1);
+}
+/**
+ * is_palindrome_helper - recursive helper function for is_palindrome
+ * @s: the string to check
+ * @start: the start index
+ * @end: the end index
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+/* base case: start index has crossed end index */
+if (start >= end)
+return (1);
+/* if start and end characters don't match, not a palindrome */
+if (s[start] != s[end])
+return (0);
+/* recurse with updated start and end indices */
+return (is_palindrome_helper(s, start + 1, end - 1));
+}
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: the string to check
@@ -7,18 +38,9 @@
  */
 int is_palindrome(char *s)
 {
-int len = 0, i;
+int len;
 if (s == NULL)
 return (0);
-/* find length of string */
-while (s[len] != '\0')
-len++;
-/* iterate over the first half of string */
-for (i = 0; i < len / 2; i++)
-{
-/* compare ith and (len-i-1)th characters */
-if (s[i] != s[len - i - 1])
-return (0);
-}
-return (1);
+len = _strlen(s);
+return (is_palindrome_helper(s, 0, len - 1));
 }
