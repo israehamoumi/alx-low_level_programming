03d582c55ee3118ce2937725ea4707b252834dc9
israehamoumi <israe.hamoumi@gmail.com>
2023-04-06 14:46:51 -0700

    israe

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 /**
- * _print_rev_recursion - Prints a string in reverse, recursively.
- *
- * @s: A pointer to the string to be printed in reverse.
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: pointer to the string to be printed in reverse.
  *
  * Return: void.
  */
 void _print_rev_recursion(char *s)
 {
-/* Base case: If s points to the null character, return. */
-if (*s == '\0')
-{
+if (*s == '\0') /* base case: reached end of string */
 return;
-}
-/* Recursive case: Call the function with the next character in the string,
- * then print the current character. */
-_print_rev_recursion(s + 1);
-_putchar(*s);
+_print_rev_recursion(s + 1); /* recursively calls function with next character */
+printf("%c", *s); /* prints the character after all recursive calls have been made */
 }
